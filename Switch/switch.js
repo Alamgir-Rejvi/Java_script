f4c6837bc14a725d0505b49ea9e54598d016bdb0
AlamgirRejvi <alamgirrejvi65@gmail.com>
2025-05-18 15:00:00 +0600

    new javascript file

---
+++ Switch/switch.js
@@ -0,0 +1,14 @@
+let input ;
+input ="no";
+
+switch(input){
+    case "yes":
+    document.writeln("continue...");
+    break;
+
+    case "no":
+    document.writeln("end...");
+    break;
+    default:
+        document.writeln("worng input")
+}
